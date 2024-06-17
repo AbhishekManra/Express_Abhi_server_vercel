104236a1da0f7903d28b19d89aa161b33b1f56f7
AbhishekManra <abhishek.manral2021@vitbhopal.ac.in>
2024-06-16 17:23:04 -0700

    addedRoute+1

--- index.js
+++ index.js
@@ -100,6 +100,37 @@ app.post("/generatecomment", async (req, res) => {
     res.status(500).json({ error: "An error occurred in gemni code" });
   }
 });
+app.post("/generatemessage", async (req, res) => {
+  try {
+    const genAI = new GoogleGenerativeAI("AIzaSyCpNiz9zXnzI2XkL8U26xH0-VMwH9mg9ig");
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+    // Get the prompt from the request body
+    const request_data  = req.body.data;
+    console.log(request_data);
+
+    const prompt = " Goals : "+  request_data[0].goals +" Tone : "+ request_data[1].tone  + " Prompt : " + request_data[3].prompt + " last message: " + request_data[5].Content + 
+      `\n\n  Above is given the Goals,tone,persona,prompt, and last message of the user, Now create a linkedIn message from my behalf use my persona to mimic me .keep it short and dont add anything extra , i am adding some of my information , this is my info ${request_data[2].persona} also give the response in a formatted manner\n `;
+      console.log(prompt)
+
+
+    // Check if the prompt is provided
+    if (!prompt) {
+      return res.status(400).json({ error: "Prompt is required" });
+    }
+
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+
+    // Send the response after all the operations have completed
+    res.json({ message: text });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred in gemni code" });
+  }
+});
 
 app.listen(3000, () => {
     console.log(`Starting Server on Port ${port}`);
