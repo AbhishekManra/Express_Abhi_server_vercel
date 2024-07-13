1058d1b111529e7bdfecf9c6ddf653fab330f1b1
AbhishekManra <abhishek.manral2021@vitbhopal.ac.in>
2024-07-13 22:23:57 +0530

    feat(routes): add new route for fetching posts

--- index.js
+++ index.js
@@ -69,6 +69,37 @@ app.post("/generatepost", async (req, res) => {
     res.status(500).json({ error: "An error occurred in gemni code" });
   }
 });
+app.post("/generatePost", async (req, res) => {
+  try {
+    const genAI = new GoogleGenerativeAI(process.env.genai_key);
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+    // Get the prompt from the request body
+    const request_data  = req.body.data;
+    console.log(request_data);
+
+    const prompt = " Goals : "+  request_data.goals +" Tone : "+ request_data.tone + " Prompt : " + request_data.prompt + 
+      `\n\n  Above is given the Goals , tone , persona and prompt, Now create a linkedIn post from the above info and also include emojis if possible ,  i am adding some of my information ,  also give the response in a formatted manner and dont give headings just give content without headings\n `;
+      console.log(prompt)
+
+
+    // Check if the prompt is provided
+    if (!prompt) {
+      return res.status(400).json({ error: "Prompt is required" });
+    }
+
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+
+    // Send the response after all the operations have completed
+    res.json({ message: text });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred in gemni code" });
+  }
+});
 
 app.post("/generatecomment", async (req, res) => {
   try {
