25f232c8f3dc943a411078e5c2b95b79a18318cd
AbhishekManra <abhishek.manral2021@vitbhopal.ac.in>
2024-09-24 17:19:39 +0530

    fix : Cors Middleware

--- index.mjs
+++ index.mjs
@@ -8,11 +8,25 @@ import cors from 'cors'
 
 const app = express();
 
-
-const port = 3000;
-
 app.use(express.json());
 app.use(cors())
+
+app.use( (req, res, next)=>{
+
+  
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  
+  next();
+});
 
 
 app.post('/generate', async (req, res) => {
