3ad027f129d4990f71dbbb0bbf4dd1c9d58f3145
AbhishekManra <abhishek.manral2021@vitbhopal.ac.in>
2024-09-24 17:40:03 +0530

    fix : added headers vercelJson

--- index.mjs
+++ index.mjs
@@ -14,16 +14,25 @@ app.use(cors())
 app.use( (req, res, next)=>{
 
   
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.linkedin.com');
+  // res.setHeader('Access-Control-Allow-Origin', 'https://www.linkedin.com/*');
 
   
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
   
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
   
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  // res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   
   next();
 });
