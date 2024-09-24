9ad7b26ba5bbc26f5b8bbba8a374142569c6b656
AbhishekManra <abhishek.manral2021@vitbhopal.ac.in>
2024-09-24 17:22:16 +0530

    fix : Add linkedIn in Header

--- index.mjs
+++ index.mjs
@@ -14,7 +14,7 @@ app.use(cors())
 app.use( (req, res, next)=>{
 
   
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.linkedin.com');
 
   
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
