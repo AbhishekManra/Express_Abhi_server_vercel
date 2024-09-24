910fe07570a8dfbc90565f6c2f49ff9e9967a031
AbhishekManra <abhishek.manral2021@vitbhopal.ac.in>
2024-09-25 00:07:58 +0530

    fix : cors middleware

--- index.mjs
+++ index.mjs
@@ -10,31 +10,33 @@ const app = express();
 
 app.use(express.json());
 
-app.use( (req, res, next)=>{
+app.use(cors())
+
+// app.use( (req, res, next)=>{
 
   
-  // res.setHeader('Access-Control-Allow-Origin', 'https://www.linkedin.com/*');
+//   // res.setHeader('Access-Control-Allow-Origin', 'https://www.linkedin.com/*');
 
   
-  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
   
-  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
   
-  // res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
+//   // res.setHeader('Access-Control-Allow-Credentials', true);
+//   res.setHeader('Access-Control-Allow-Credentials', true)
+//   res.setHeader('Access-Control-Allow-Origin', '*')
+//   // another common pattern
+//   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+//   )
   
-  next();
-});
+//   next();
+// });
 
 
 app.post('/generate', async (req, res) => {
